4f84c3e7fadbf87e4a758bc383960690a2338a0f
IVB107 <ianvb107@gmail.com>
2019-07-15 17:45:37 -0500

    Added Grid component

--- game/src/index.js
+++ game/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Grid from './components/Grid'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Main extends Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor() {
+    super()
+    this.state = {
+      generation: 0
+    }
+  }
+
+  render () {
+    return (
+      <>
+        <h1>The Game of Life</h1>
+        <Grid></Grid>
+        <h2>Generations: {this.state.generation}</h2>
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
